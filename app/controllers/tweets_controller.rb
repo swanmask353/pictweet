113bbd5efbae69c6c414d23fd3e17d2b2beecfe9
swanmask353 <youhei.suwa@gmail.com>
2024-04-14 20:35:18 +0900

    詳細ページ表示の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+  
   def index
     @tweets = Tweet.all
   end
@@ -19,8 +21,8 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
+
 
   def update
     tweet = Tweet.find(params[:id])
@@ -28,10 +30,16 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+  end
 
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 
 end
